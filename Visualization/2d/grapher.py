40ab335f5531f5ef9fe1fed807f93351c144af53
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-08-19 15:48:56 -0300

    chore: modified graphers to work with new json

--- Visualization/2d/grapher.py
+++ Visualization/2d/grapher.py
@@ -22,35 +22,46 @@ def get_color(value):
 # Custom colormap: 0 -> white, 1 -> green
 cmap = ListedColormap(['white', 'green'])
 
-# List to store images for GIF
-images = []
+for i, simulation in enumerate(data):
+    params=simulation['params']
+    dimension=params['dimension']
+    
+    if dimension=='2D':
+        initializationPercentage= params['initializationPercentage']
+        ruleset=params['ruleset']
+        amountToRevive=ruleset['amountToRevive']
+        neighboursToDie1=ruleset['neighboursToDie1']
+        neighboursToDie2=ruleset['neighboursToDie2']
+        # List to store images for GIF
+        images = []
 
-# Visualize the grid for each evolution
-for evolution in data['results']:
-    for key, grid in evolution.items():
-        # Convert the grid to a numpy array for easy manipulation
-        grid_array = np.array(grid)
+        # Visualize the grid for each evolution
+        for evolution in simulation['results']:
+            for key, grid in evolution.items():
+                # Convert the grid to a numpy array for easy manipulation
+                grid_array = np.array(grid)
 
-        # Plot the grid
-        fig, ax = plt.subplots()
-        ax.imshow(grid_array, cmap=cmap, vmin=0, vmax=1)
+                # Plot the grid
+                fig, ax = plt.subplots()
+                ax.imshow(grid_array, cmap=cmap, vmin=0, vmax=1)
 
-        # Hide grid lines
-        ax.grid(False)
+                # Hide grid lines
+                ax.grid(False)
 
-        # Hide axes ticks
-        ax.set_xticks([])
-        ax.set_yticks([])
+                # Hide axes ticks
+                ax.set_xticks([])
+                ax.set_yticks([])
 
-        # Set the title
-        ax.set_title(key)
+                # Set the title
+                ax.set_title(key)
 
-        # Save the current figure to an image in memory
-        plt.savefig("temp_image.png")
-        images.append(imageio.imread("temp_image.png"))
+                #TODO add params to GIF
+                # Save the current figure to an image in memory
+                plt.savefig("temp_image.png")
+                images.append(imageio.imread("temp_image.png"))
 
-        # Close the plot to avoid display during the process
-        plt.close()
+                # Close the plot to avoid display during the process
+                plt.close()
 
-# Create a GIF from the collected images
-imageio.mimsave('simulation_evolution.gif', images, duration=configuration['delaySeconds'] * SECONDS_MULTIPLIER)
+        # Create a GIF from the collected images
+        imageio.mimsave('simulation_evolution'+i+'.gif', images, duration=configuration['delaySeconds'] * SECONDS_MULTIPLIER)
